7288ff7a25b2612cd41350c2a81b88749a43d9ad
Ian <i.r.butterworth@gmail.com>
2019-08-07 16:49:53 -0400

    Update init.jl

--- src/init.jl
+++ src/init.jl
@@ -1,1 +1,8 @@
-init.
+# Load in `deps.jl`, complaining if it does not exist
+const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if !isfile(depsjl_path)
+    println("Deps path: $depsjl_path")
+    error("FFMPEG not installed properly, run `] build FFMPEG`, restart Julia and try again")
+end
+
+include(depsjl_path)
