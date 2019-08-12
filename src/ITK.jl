4e705b7868ca33651a91f8ab3bb926e4c6085977
Connor <cjmclaughlin@wpi.edu>
2019-08-12 11:36:40 -0400

    bug fix and docstring format

--- src/ITK.jl
+++ src/ITK.jl
@@ -1,36 +1,41 @@
 module ITK
 using Cxx, Libdl
 
-#if !isfile(wrapjl_path)
-#    error("ITK not properly installed. Run `] build ITK`, restart Julia and try again")
-#end
-
 include("init.jl")
 
 function __init__()
     loadcxx(libitk, dirname(libitk))
 end
 
-# Loads shared library and header file
+"Loads shared library and header file."
 function loadcxx(libpath::String=libitk, headerdir::String=dirname(libitk))
     addHeaderDir(headerdir, kind=C_System)
     Libdl.dlopen(libpath, Libdl.RTLD_GLOBAL)
     cxxinclude("JuliaWrap.h")
 end
 
-# Sanity check. Returns x if able to read C++ files
+"Sanity checker. Returns x if able to read C++ files."
 function verifycxx(x::Int)
     sanity_check(n::Int) = @cxx sanity(n)
     return sanity_check(x)
 end
 
-# Initial registration test method - performs translation registration with MattesMutualInformation Metric, and specified optimizer.
-# fixedImage -> String path of fixed image file
-# movingImage -> String path of moving image file
-# outputImage -> String path of desired output image file (can be blank if writeImage=false)
-# writeImage -> Boolean, whether or not to save resulting registered image. Default=true.
-# optimizer -> String, "Gradient" | "Amoeba". Default="Gradient"
-# Return: (x translation, y translation, metric info)
+"""
+    registerframe(fixedImage, movingImage, outputImage, writeImage, optimizer)
+
+Test method which registers movingImage in relation to fixedImage, writing the resulting image to outputImage if writeImage is true.
+Uses Mattes Mutual Information Metric, and either Regular Step Gradient Descent or Amoeba optimizer.
+
+# Arguments
+- `fixedImage::String`: path to fixed image file
+- `movingImage::String`: path to moving image file
+- `outputImage::String`: path to desired output image file
+- `writeImage::Bool=true`: whether or not to save the output image file
+- `optimizer::String="Gradient"`: which optimizer to use, "Gradient" | "Amoeba"
+
+# Returns
+- (x translation, y translation, metric information)
+"""
 function registerframe(fixedImage::String, movingImage::String, outputImage::String, writeImage::Bool=true, optimizer::String="Gradient")
     register_amoeba_optimizer(fix::Ptr{UInt8}, moving::Ptr{UInt8}, output::Ptr{UInt8}, write::Bool) = @cxx test_registration1(fix, moving, output, write)
     register_gradient_optimizer(fix::Ptr{UInt8}, moving::Ptr{UInt8}, output::Ptr{UInt8}, write::Bool) = @cxx test_registration2(fix, moving, output, write)
@@ -43,15 +48,24 @@ function registerframe(fixedImage::String, movingImage::String, outputImage::Str
     return x, y, metric
 end
 
-# Another registration method using MattesMutualInformation metric, with Gradient Descent optimizer with specified parameters.
-# fixedImage -> String path of fixed image file
-# movingImage -> String path of moving image file
-# outputImage -> String path of desired output image file (can be blank if writeImage=false)
-# writeImage -> Boolean, whether or not to save resulting registered image. 
-# learningRate -> Recommended values [1:5], higher values result in quicker but less stable registration
-# minStepLength -> Recommended values [0.05], higher values result in quicker but less stable registration
-# maxIterations -> Recommended values [100:200], threshold number of iterations before returning translation
-# relaxationFactor -> Recommended values [~0.5], factor used to slow down shifts between iterations, higher values result in quicker but potentially divergent registration
+"""
+    MMIGradientRegistration(fixedImage, movingImage, outputImage, writeImage, learningRate, minStepLength, maxIterations, relaxationFactor) 
+
+Register movingImage to fixedImage with Mattes Mutual Information metric and Regular Step Gradient Descent optimizer.
+
+# Arguments
+- `fixedImage::String`: path to fixed image file
+- `movingImage::String`: path to moving image file
+- `outputImage::String`: path to desired output image file
+- `writeImage::Bool`: whether or not to save the output image file
+- `learningRate::Float64`: higher values result in quicker but potentially unstable registration (recommended range 1.0:5.0)
+- `minStepLength::Float64`: step size (recommended range 0.001:0.05)
+- `maxIterations::Int64`: max number of iterations (recommended range 100:200)
+- `relaxationFactor::Float64`: factor to slow down shift between iterations, higher values result in quicker, potentially unstable registration (recommended range 0.3:0.7)
+
+# Returns
+- (x translation, y translation, metric information)
+"""
 function MMIGradientRegistration(fixedImage::String, movingImage::String, outputImage::String, writeImage::Bool, learningRate::Float64, minStepLength::Float64, maxIterations::Int64, relaxationFactor::Float64)
     register(fix::Ptr{UInt8}, moving::Ptr{UInt8}, output::Ptr{UInt8}, write::Bool, LR::Float64, minStep::Float64, maxIter::Int64, relaxFactor::Float64) = @cxx MMIGradientTranslation(fix, moving, output, write, LR, minStep, maxIter, relaxFactor)
     result = register(pointer(fixedImage), pointer(movingImage), pointer(outputImage), writeImage, learningRate, minStepLength, maxIterations, relaxationFactor)
@@ -59,15 +73,24 @@ function MMIGradientRegistration(fixedImage::String, movingImage::String, output
     return x, y, metric
 end
 
-# Another registration method using MattesMutualInformation metric, with Amoeba optimizer with specified parameters.
-# fixedImage -> String path of fixed image file
-# movingImage -> String path of moving image file
-# outputImage -> String path of desired output image file (can be blank if writeImage=false)
-# writeImage -> Boolean, whether or not to save resulting registered image.
-# initialSimplex -> Recommended values [5.0], initial size of simplex moving over cost surface 
-# pixelTolerance -> Optimizer convergence tolerance, pixels
-# metricTolerance -> Optimizer function convergence tolerance, bits
-# maxIterations -> maximum number of iterations
+"""
+    MMIAmoebaRegistration(fixedImage, movingImage, outputImage, writeImage, initialSimplex, pixelTolerance, metricTolerance, maxIterations)
+
+Registers movingImage to fixedImage with MattesMutualInformation metric, with Amoeba optimizer with specified parameters.
+
+# Arguments
+- `fixedImage::String`: path to fixed image file
+- `movingImage::String`: path to moving image file
+- `outputImage::String`: path to desired output image file
+- `writeImage::Bool`: whether or not to save the output image file
+- `initialSimplex::Float64`: initial size of simplex moving along cost surface (recommended 5.0)
+- `pixelTolerance::Float64`: convergence tolerance in pixels (recommended range 0.01:0.5)
+- `metricTolerance::Float64`: function convergence tolerance (recommended 0.1)
+- `maxIterations::Int64`: max number of iterations (recommended range 100:200)
+
+# Returns
+- (x translation, y translation, metric information)
+"""
 function MMIAmoebaRegistration(fixedImage::String, movingImage::String, outputImage::String, writeImage::Bool, initialSimplex::Float64, pixelTolerance::Float64, metricTolerance::Float64, maxIterations::Int64)
     register(fix::Ptr{UInt8}, moving::Ptr{UInt8}, output::Ptr{UInt8}, write::Bool, simplex::Float64, pixelT::Float64, metricT::Float64, maxIter::Int64) = @cxx MMIAmoebaTranslation(fix, moving, output, write, simplex, pixelT, metricT, maxIter)
     result = register(pointer(fixedImage), pointer(movingImage), pointer(outputImage), writeImage, initialSimplex, pixelTolerance, metricTolerance, maxIterations)
@@ -75,27 +98,48 @@ function MMIAmoebaRegistration(fixedImage::String, movingImage::String, outputIm
     return x, y, metric
 end
 
-# Another registration method using MattesMutualInformation metric, with 1+1 Evolutionary optimizer with specified parameters.
-# fixedImage -> String path of fixed image file
-# movingImage -> String path of moving image file
-# outputImage -> String path of desired output image file (can be blank if writeImage=false)
-# writeImage -> Boolean, whether or not to save resulting registered image. 
-function MMIOnePlusOneRegistration(fixedImage::String, movingImage::String, outputImage::String, writeImage::Bool, initialize::Float64, epsilon::Float64, maxIterations::Int64)
+"""
+    MMIOnePlusOneRegistration(fixedImage, movingImage, outputImage, writeImage, initialize, epsilon, maxIterations)
+
+Registers movingImage to fixedImage with MattesMutualInformation metric, with 1+1 Evolutionary optimizer with specified parameters.
+
+# Arguments
+- `fixedImage::String`: path to fixed image file
+- `movingImage::String`: path to moving image file
+- `outputImage::String`: path to desired output image file
+- `writeImage::Bool`: whether or not to save the output image file
+- `initialRadius::Float64`: initial size of radius (recommended 6.25e-3)
+- `epsilon::Float64`: minimum size of search radius (recommended 1.5e-6)
+- `maxIterations::Int64`: max number of iterations (recommended range 100:200)
+
+# Returns
+- (x translation, y translation, metric information)
+"""
+function MMIOnePlusOneRegistration(fixedImage::String, movingImage::String, outputImage::String, writeImage::Bool, initialRadius::Float64, epsilon::Float64, maxIterations::Int64)
     register(fix::Ptr{UInt8}, moving::Ptr{UInt8}, output::Ptr{UInt8}, write::Bool, init::Float64, ep::Float64, maxIter::Int64) = @cxx MMIOnePlusOneTranslation(fix, moving, output, write, init, ep, maxIter)
-    result = register(pointer(fixedImage), pointer(movingImage), pointer(outputImage), writeImage, initialSimplex, pixelTolerance, metricTolerance, maxIterations)
+    result = register(pointer(fixedImage), pointer(movingImage), pointer(outputImage), writeImage, initialRadius, epsilon, maxIterations)
     x, y, metric = unsafe_load(result, 1), unsafe_load(result, 2), unsafe_load(result, 3)
     return x, y, metric
 end
 
-# Another registration method using Mean Squares metric, with Gradient Descent optimizer with specified parameters.
-# fixedImage -> String path of fixed image file
-# movingImage -> String path of moving image file
-# outputImage -> String path of desired output image file (can be blank if writeImage=false)
-# writeImage -> Boolean, whether or not to save resulting registered image. 
-# learningRate -> Recommended values [1:5], higher values result in quicker but less stable registration
-# minStepLength -> Recommended values [0.05], higher values result in quicker but less stable registration
-# maxIterations -> Recommended values [100:200], threshold number of iterations before returning translation
-# relaxationFactor -> Recommended values [~0.5], factor used to slow down shifts between iterations, higher values result in quicker but potentially divergent registration
+"""
+MeanSquaresGradientRegistration(fixedImage, movingImage, outputImage, writeImage, learningRate, minStepLength, maxIterations, relaxationFactor)
+
+Register movingImage to fixedImage with Mean Squares metric and Regular Step Gradient Descent optimizer.
+
+# Arguments
+- `fixedImage::String`: path to fixed image file
+- `movingImage::String`: path to moving image file
+- `outputImage::String`: path to desired output image file
+- `writeImage::Bool`: whether or not to save the output image file
+- `learningRate::Float64`: higher values result in quicker but potentially unstable registration (recommended range 1.0:5.0)
+- `minStepLength::Float64`: step size (recommended range 0.001:0.05)
+- `maxIterations::Int64`: max number of iterations (recommended range 100:200)
+- `relaxationFactor::Float64`: factor to slow down shift between iterations, higher values result in quicker, potentially unstable registration (recommended range 0.3:0.7)
+
+# Returns
+- (x translation, y translation, metric information)
+"""
 function MeanSquaresGradientRegistration(fixedImage::String, movingImage::String, outputImage::String, writeImage::Bool, learningRate::Float64, minStepLength::Float64, maxIterations::Int64, relaxationFactor::Float64)
     register(fix::Ptr{UInt8}, moving::Ptr{UInt8}, output::Ptr{UInt8}, write::Bool, LR::Float64, minStep::Float64, maxIter::Int64, relaxFactor::Float64) = @cxx MeanSquaresGradientTranslation(fix, moving, output, write, LR, minStep, maxIter, relaxFactor)
     result = register(pointer(fixedImage), pointer(movingImage), pointer(outputImage), writeImage, learningRate, minStepLength, maxIterations, relaxationFactor)
