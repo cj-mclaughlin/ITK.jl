1701e1058bf6f88f4beadc0b38bf2515b34fde77
Connor <cjmclaughlin@wpi.edu>
2019-08-12 14:45:00 -0400

    testing new test method

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,19 @@
 using ITK
 using Test
-#using TestImages
+using TestImages
 
 @testset "Sanity Check" begin
     @test ITK.verifycxx(0) == 0
     @test ITK.verifycxx(100) == 100
 end
 
-# @testset "Registration Control Check" begin
-#    testimage_path = string(joinpath(dirname(dirname(pathof(TestImages)))), "/images")
-#     lighthouse_path = joinpath(testimg_path, "lighthouse.png")
+@testset "Registration Control Check" begin
+    testimage_path = string(joinpath(dirname(dirname(pathof(TestImages)))), "/images")
+    lighthouse_path = joinpath(testimg_path, "lighthouse.png")
 
-#     amoeba_control = ITK.registerframe(lighthouse_path, lighthouse_path, "NA", false, "Amoeba")
-#     gradient_control = ITK.registerframe(lighthouse_path, lighthouse_path, "NA", false, "Gradient")
+    testreg1 = ITK.registerframe(lighthouse_path, lighthouse_path, "NA", false, "Amoeba")
+    testreg2 = ITK.registerframe(lighthouse_path, lighthouse_path, "NA", false, "Gradient")
 
-#     @test (amoeba_control[1], amoeba_control[2]) == (0, 0)
-#     @test (gradient_control[1], gradient_control[2]) == (0, 0)
-# end
+    @test (testreg1[1], testreg2[2]) == (0, 0)
+    @test (testreg2[1], testreg2[2]) == (0, 0)
+end
