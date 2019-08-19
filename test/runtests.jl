61af8de1372364a6b50d06535e37358166b01a67
Connor <cjmclaughlin@wpi.edu>
2019-08-19 16:08:57 -0400

    initial testcase check

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,16 +8,16 @@ using TestImages
     @test ITK.verifycxx(100) == 100
 end
 
-# Simple registration check on still image
-# @testset "Registration Control Check" begin
-#     testimage_path = string(joinpath(dirname(dirname(pathof(TestImages)))), "/images")
-#     lighthouse_path = joinpath(testimage_path, "lighthouse.png")
+Simple registration check on still image
+@testset "Registration Control Check" begin
+    testimage_path = string(joinpath(dirname(dirname(pathof(TestImages)))), "/images")
+    lighthouse_path = joinpath(testimage_path, "lighthouse.png")
 
-#     testreg1 = ITK.registerframe(lighthouse_path, lighthouse_path, "NA", false, "Amoeba")
-#     testreg2 = ITK.MeanSquaresGradientRegistration(lighthouse_path, lighthouse_path, "NA", false, 5.0, 0.005, 30, 0.5)
+    #testreg1 = ITK.registerframe(lighthouse_path, lighthouse_path, "NA", false, "Amoeba")
+    testreg2 = ITK.MeanSquaresGradientRegistration(lighthouse_path, lighthouse_path, "NA", false, 5.0, 0.005, 30, 0.5)
 
-#     @test (testreg1[1], testreg1[2]) == (0, 0)
-#     @test (testreg2[1], testreg2[2]) == (0, 0)
-# end
+    #@test (testreg1[1], testreg1[2]) == (0, 0)
+    @test (testreg2[1], testreg2[2]) == (0, 0)
+end
 
 # TODO registration test case on moving images
